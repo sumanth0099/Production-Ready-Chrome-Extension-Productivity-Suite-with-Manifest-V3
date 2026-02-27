84ab11d9a3875241a547aa390228d81b8c0ed937
sumanth0099 <tetalasumanthreddy@gmail.com>
2026-02-27 21:49:54 +0530

    added options.jsx

---
+++ src/options.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import OptionsApp from './components/OptionsApp';
+import './styles/options.css';
+
+ReactDOM.createRoot(document.getElementById('options-root')).render(
+  <React.StrictMode>
+    <OptionsApp />
+  </React.StrictMode>
+);
